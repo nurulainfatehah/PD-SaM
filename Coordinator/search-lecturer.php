b673f7d3a2636989d4ee905a844a35a7b078deac
B032020035 NURUL AIN FATEHAH BINTI MOHD SHAM <80867908+nurulainfatehah@users.noreply.github.com>
2023-06-06 02:20:40 +0800

    pop-up view of lecturer
    create pop-up window of lecturer record with list of supervised and evaluation list

--- Coordinator/search-lecturer.php
+++ Coordinator/search-lecturer.php
@@ -10,21 +10,21 @@ if(isset($_POST['querySearchLecturer'])){
 	$querySearchLecturer = "";
 
 	if($_POST['opt'] == "idAZ"){
-		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR status LIKE '%".$search."%' or email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID";
+		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID";
 	}else if($_POST['opt'] == "idZA"){
-		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR status LIKE '%".$search."%' or email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID DESC";
+		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID DESC";
 	}else if($_POST['opt'] == "nameAZ"){
-		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR status LIKE '%".$search."%' or email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY name";
+		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY name";
 	}else if($_POST['opt'] == "nameZA"){
-		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR status LIKE '%".$search."%' or email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY name DESC";
+		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY name DESC";
 	}else if($_POST['opt'] == "roleCoordinator"){
-		$querySearch = "SELECT * FROM lecturer WHERE role = 'COORDINATOR' AND (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR status LIKE '%".$search."%' or email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID DESC";
+		$querySearch = "SELECT * FROM lecturer WHERE role = 'COORDINATOR' AND (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID DESC";
 	}else if($_POST['opt'] == "roleCoordinator"){
-		$querySearch = "SELECT * FROM lecturer WHERE role = 'COORDINATOR' AND (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR status LIKE '%".$search."%' or email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID DESC";
+		$querySearch = "SELECT * FROM lecturer WHERE role = 'COORDINATOR' AND (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID DESC";
 	}else if($_POST['opt'] == "roleSupervisor"){
-		$querySearch = "SELECT * FROM lecturer WHERE role = 'SUPERVISOR' AND (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR status LIKE '%".$search."%' or email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID DESC";
+		$querySearch = "SELECT * FROM lecturer WHERE role = 'SUPERVISOR' AND (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID DESC";
 	}else{
-		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR status LIKE '%".$search."%' or email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID";
+		$querySearch = "SELECT * FROM lecturer WHERE (name LIKE '%".$search."%' OR lecturerID LIKE '%".$search."%' OR email LIKE '%".$search."%' OR officePhone LIKE '%".$search."%') ORDER BY lecturerID";
 	}
 }else{
 		$querySearch = "SELECT * FROM lecturer ORDER BY lecturerID";
@@ -35,6 +35,26 @@ $result = $conn->query($querySearch);
 if($result->num_rows > 0)
 {
 	?>
+	<script type="text/javascript">
+		function viewLecturer(obj, name){
+			var name = name;
+			var lecturerID = obj.id;
+			$.confirm({
+				boxWidth: '60%',
+				useBootstrap: false,			
+				title: name,
+				cancelButton: false,
+				content: 'url:../ajaxprocess/viewLecturer.php?lecturerID=' + lecturerID,
+				buttons: {
+					ok:{        
+						isHidden: true, // hide the button       
+					},
+					 close: function () {
+        			}
+				}
+			});
+		}
+	</script>
 	<a id="print-btn" target="_blank" href="print/print-lecturer-list.php"><button>Print</button></a><br><br>
 	<table align="center">
 		<tr>
@@ -42,60 +62,46 @@ if($result->num_rows > 0)
 			<th width="10%">Staff ID</th>
 			<th>Full Name</th>
 			<th width="13%" >Office No.</th>
-			<th width="10%">Status</th>
 			<th width="15%">Role</th>
 			<th width="10%">Action</th>
 		</tr>
 	<?php
 	while($row = $result->fetch_assoc()){
-		$sqlFKassociated = "SELECT * FROM lecturer WHERE lecturerID='".$row['lecturerID']."' AND lecturer.lecturerID IN (SELECT supervision_request.lecturerID FROM supervision_request)" ;
+		$sqlFKassociated = "SELECT * FROM lecturer WHERE lecturerID='".$row['lecturerID']."' AND ((lecturer.lecturerID IN (SELECT supervisorID FROM student)) OR (lecturer.lecturerID IN (SELECT evaluatorID FROM student)) OR (lecturer.lecturerID IN (SELECT supervision_request.lecturerID FROM supervision_request)))" ;
 		$resultFK = $conn->query($sqlFKassociated);
 		$total++;
-		if($row['lecturerID'] == $usernameInSession){
-			?>
-			<tr>
+		?>
+		<tr>
 				<td align="center"><?php echo $total ?></td>
 				<td align="center"><?php echo $row['lecturerID'] ?></td>
 				<td><?php echo $row['name'] ?></td>
 				<td align="center"><?php echo $row['officePhone'] ?></td>
-				<td align="center"><?php echo $row['status'] ?></td>
 				<td align="center"><?php echo $row['role'] ?></td>
 				<td align="center">
-					<a title="view <?php echo $row['name'] ?>" onclick="viewLecturer(this)" id="<?php echo $row["lecturerID"] ?>"><i class="fa fa-search" aria-hidden="true"></i></a>
-				</td>
-			</tr>
+		<?php
+		if($row['lecturerID'] == $usernameInSession){
+			?>
+					<a  style="cursor: default; color: gray" ><i title="Cannot remove your own account" class="fa fa-trash" aria-hidden="true"></i></a>
+					<a title="view <?php echo $row['name'] ?>" onclick="viewLecturer(this, '<?php echo $row["name"] ?>')" id="<?php echo $row["lecturerID"] ?>"><i class="fa fa-search" aria-hidden="true"></i></a>
+				
 			<?php
 		}else if($resultFK->num_rows > 0){
 			?>
-			<tr>
-				<td align="center"><?php echo $total ?></td>
-				<td align="center"><?php echo $row['lecturerID'] ?></td>
-				<td><?php echo $row['name'] ?></td>
-				<td align="center"><?php echo $row['officePhone'] ?></td>
-				<td align="center"><?php echo $row['status'] ?></td>
-				<td align="center"><?php echo $row['role'] ?></td>
-				<td align="center">
+			
 					<a  style="cursor: default; color: gray" ><i title="Lecturer cannot be removed as it associates with important record" class="fa fa-trash" aria-hidden="true"></i></a>
-					<a href="#table_lecturer" title="view <?php echo strtolower($row['name']) ?>" onclick="viewLecturer(this)" id="<?php echo $row["lecturerID"] ?>"><i class="fa fa-search" aria-hidden="true"></i></a>
-				</td>
-			</tr>
+					<a href="#table_lecturer" title="view <?php echo strtolower($row['name']) ?>" onclick="viewLecturer(this, '<?php echo $row["name"] ?>')" id="<?php echo $row["lecturerID"] ?>"><i class="fa fa-search" aria-hidden="true"></i></a>
 			<?php
 		}else{
 		?>
-			<tr>
-				<td align="center"><?php echo $total ?></td>
-				<td align="center"><?php echo $row['lecturerID'] ?></td>
-				<td><?php echo $row['name'] ?></td>
-				<td align="center"><?php echo $row['officePhone'] ?></td>
-				<td align="center"><?php echo $row['status'] ?></td>
-				<td align="center"><?php echo $row['role'] ?></td>
-				<td align="center">
 					<a title="remove <?php echo strtolower($row['name']) ?>" onclick="deleteLecturer(this, '<?php echo $row["name"] ?>')" id="<?php echo $row['lecturerID'] ?>" class="oppose"><i class="fa fa-trash" aria-hidden="true"></i></a>
-					<a href="#table_lecturer" title="view <?php echo strtolower($row['name']) ?>" onclick="viewLecturer(this)" id="<?php echo $row['lecturerID'] ?>" ><i class="fa fa-search" aria-hidden="true"></i></a>
-				</td>
-			</tr>
+					<a href="#table_lecturer" title="view <?php echo strtolower($row['name']) ?>" onclick="viewLecturer(this, '<?php echo $row["name"] ?>')" id="<?php echo $row['lecturerID'] ?>" ><i class="fa fa-search" aria-hidden="true"></i></a>
 		<?php
 		}
+
+		?>
+			</td>
+		</tr>
+		<?php
 	}
 
 	?>
---
+++ ajaxprocess/viewLecturer.php
@@ -0,0 +1,188 @@
+<?php
+include('../inc/dbconnect.php');
+session_start();
+
+if(isset($_GET['lecturerID'])){
+	$lecturerID = $_GET['lecturerID'];
+
+	$sqlLec = "SELECT * FROM lecturer WHERE lecturerID = '".$lecturerID."'";
+	$resultLec = $conn->query($sqlLec);
+	$row = $resultLec->fetch_assoc();
+
+	$sqlSVList = "SELECT * FROM student WHERE supervisorID = '".$lecturerID."'";
+	$resultSVList = $conn->query($sqlSVList);
+
+	$sqlEVList = "SELECT * FROM student WHERE evaluatorID = '".$lecturerID."'";
+	$resultEVList = $conn->query($sqlEVList);
+
+	?>
+
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="../css/view-profile-popup.css">
+		<link rel="stylesheet" type="text/css" href="../css/header.css">
+		<link rel="stylesheet" type="text/css" href="../css/dashboard.css">
+		<title><?php $row['name'] ?></title>
+		<style type="text/css">
+
+		</style>
+	</head>
+	<body >
+		<hr>
+		<div align="center" style="width: 90%; margin-left: auto; margin-right: auto;">
+			<div class="grid-container">
+				<div class="grid-child purple" style="padding-top: 10%; padding-bottom: 20%;">
+					<!--Grid Column 1-->
+
+					<?php
+					if(empty($row['picture']))
+					{
+						?>
+						<div class="profile-picnohoverbig" style="background-image: url('../attachment/imgsource/lecturer1.png'); margin-top: 10%; margin-bottom: 10%" >
+							<?php 
+						}
+						else
+						{
+							?>
+							<div class="profile-picnohoverbig" style="background-image: url('../attachment/profile/<?php echo $row['picture']?>');  margin-top: 10%; margin-bottom: 10%" >
+								<?php 
+							}
+							?>
+						</div>
+				</div>
+				<div class="grid-child green" style="margin-left: 2%; padding-top: 0%; padding-bottom: 0%;">
+					<!--Grid Column 2-->						
+					<div class="informationHolder" style="width:95%">
+						<?php echo $row['name']; ?>
+					</div>
+					<div class="information" style="width: 95%">
+						<table align="center" style="border: 1px solid #ccc;" cellspacing="2">
+							<tr>
+								<td style="width: 27%">
+									STAFF ID
+								</td>
+								<td>
+									<?php echo $row['lecturerID']; ?>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									E-MAIL
+								</td>
+								<td>
+									<?php echo $row['email'] ?>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									PHONE
+								</td>
+								<td>
+									<?php echo $row['officePhone'] ?>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									ROLE
+								</td>
+								<td>
+									<?php echo $row['role']; ?>
+								</td>
+							</tr>
+						</table>
+					</div>
+				</div>					  	
+			</div>
+			<?php
+			if($resultSVList->num_rows > 0){
+				?>
+				<hr>
+				<center style="font-size: 14px; font-weight: bold;">SUPERVISED LIST (<?php echo $resultSVList->num_rows ?>)</center>
+				<hr>
+				
+				<div id="main" align="center">
+					<?php
+					while ($rowSVList = $resultSVList->fetch_assoc()){
+						?>
+						<div id="list-box">
+							<?php
+							if(empty($rowSVList['picture']))
+							{
+								?>
+								<div class="pic-list" style="background-image: url('../attachment/imgsource/studenticon.png');" >
+									<?php 
+							}
+							else
+							{
+								?>
+								<div class="pic-list" style="background-image: url('../attachment/profile/<?php echo $rowSVList['picture']?>');" >
+								<?php 
+							}
+							?>
+								</div>
+							<p id="list-data">
+								<?php
+								echo '<b>' . $rowSVList['matricNo']. '</b><br> ' . $rowSVList['name'];
+								?>
+							</p>
+						</div>
+						<?php
+					}
+					?>						
+				</div>
+				<?php
+			}
+			?>
+
+			<?php
+			if($resultEVList->num_rows > 0){
+				?>
+				<hr>
+				<center style="font-size: 14px; font-weight: bold;">EVALUATION LIST (<?php echo $resultEVList->num_rows ?>)</center>
+				<hr>
+				<div id="main" align="center">
+					<?php
+					while ($rowEVList = $resultEVList->fetch_assoc()){
+						?>
+						<div id="list-box">
+							<?php
+							if(empty($rowEVList['picture']))
+							{
+								?>
+								<div class="pic-list" style="background-image: url('../attachment/imgsource/studenticon.png');" >
+									<?php 
+							}
+							else
+							{
+								?>
+								<div class="pic-list" style="background-image: url('../attachment/profile/<?php echo $rowEVList['picture']?>');" >
+								<?php 
+							}
+							?>
+								</div>
+							<p id="list-data">
+								<?php
+								echo '<b>' . $rowEVList['matricNo']. '</b><br> ' . $rowEVList['name'];
+								?>
+							</p>
+						</div>
+						<?php
+					}
+					?>						
+				</div>
+				<?php
+			}
+			?>
+			
+		</div>
+		</div>
+	</body>
+	</html> 
+	<?php
+}else{
+
+}
+
